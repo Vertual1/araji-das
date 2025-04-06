80fb4c52373d11be1a520b9662b7dba3d8ae3bc5
Vertual1 <hunanyandavid877@gmail.com>
2025-04-06 18:58:33 +0400

    change commit

--- leson1/src/das1git.java
+++ leson1/src/das1git.java
@@ -1,11 +1,9 @@
 public class das1git {
     public static void main(String[] args) {
-        System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
-        System.out.println("sdsd");
+        int a =5;
+        int b = 3;
+        int d =15;
+        int e =a + b * d;
+        System.out.println(e);
     }
 }
